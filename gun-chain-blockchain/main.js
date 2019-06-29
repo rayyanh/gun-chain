eea4874f7ee16377497e61d58e25832ac202eb73
Pranav Sakthivel <pranavsakthivel@outlook.com>
2019-06-29 13:55:45 -0700

    Co-authored-by: Sponge Gar <dinoking99@gmail.com>

--- gun-chain-blockchain/main.js
+++ gun-chain-blockchain/main.js
@@ -11,19 +11,34 @@ class Block
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash()
     {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty)
+    {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0"))
+        {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
+
+
 
 class Blockchain
 {
     constructor()
     {
         this.chain = [this.createGenesisBlock];
+        this.difficulty = 2;
     }
 
     createGenesisBlock()
@@ -39,12 +54,37 @@ class Blockchain
     addBlock(newBlock)
     {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+    }
+
+    isChainValid()
+    {
+        for (let i = 1; i < this.chain.length; i++)
+        {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash())
+            {
+                return false;
+            }
+            
+            if (currentBlock.previousHash !== previousBlock.hash)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
 
 let gunChain = new Blockchain();
+console.log('Mining block 1: ');
 gunChain.addBlock(new Block(1, "06/29/2019", { gunName: "Colt M1911", serialNo: 69, gunOwner: "John Johnson"}));
+console.log('Mining block 2: ');
 gunChain.addBlock(new Block(2, "06/29/2019", { gunName: "H&K G36", serialNo: 420, gunOwner: "Adam Adamson"}));
-console.log(JSON.stringify(gunChain, null, 4));
+
+
